3e75136718d74e5385185e23c516310402d8486c
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-02-16 16:08:38 +0200

    add a File Program.cs

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListManupulation
+{
+    class Program
+    {
+        static void Main() 
+        {
+           List<double> nums = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList(); // read an input and create List of ints
+
+            for (int i = 1; i < nums.Count; i++)
+            {                    // 3 3 6 1 // 8 16 16
+                if ( nums[i-1] == nums[i] )
+                {
+                    nums[i - 1] += nums[i];
+                    nums.RemoveAt(i);
+                    i = 0;  // assign it to  0 , we want always to compare nums[0] to next !!                  
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', nums));
+
+        }  
+    }
+}
